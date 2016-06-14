d5ebfb13473c095ac2aae026fd8c0bd3c03d7468
IngarArntzen <ingar.arntzen@gmail.com>
2016-06-14 14:31:56 +0200

    completed slidingwindow sequencing tool

--- source/sequencing/slidingwindow.js
+++ source/sequencing/slidingwindow.js
@@ -18,31 +18,104 @@ define(['util/eventutils', 'util/motionutils'],
 		
 	*/
 
-	var SlidingWindow = function (timingObject, length, options) {
+	var SlidingWindow = function (timingObject, length) {
 		this._to = timingObject;
 		this._length = length;
-		this._window;
-		options = options || {};
-		if (options.tail !== "number" || options.tail < 5) options.tail = 5;
-		if (options.head !== "number" || options.head < 5) options.head = 5;
-		this._options = options;
-		this._to.on("change", this._toHandler, this);
+		this._low;
+		this._high;
+		this._timeout = null; // timeout
+
+		// event support
+		eventutils.eventify(this, SlidingWindow.prototype);
+		this.eventifyDefineEvent("change", {init:true}); // define change event (supporting init-event)
+		this._to.on("change", this._onChange, this);
+
 	};
 
+	SlidingWindow.prototype.eventifyMakeInitEvents = function (type) {
+		if (type === "change") {
+			return [{type: type, e: undefined}]; 
+		} else {
+			return [];
+		};
+	};
+	
+	Object.defineProperty(SlidingWindow.prototype, 'window', {
+		get : function () {
+			var quarter = this._length/4.0;
+			return {low:this._low - quarter, high:this._high + quarter};
+		}
+	});
 
-	SlidingWindow.prototype._needAdvance = function (pos) {
-		if (!this._window) return true;
-		// use direction
+	// adjust window if necessary
+	SlidingWindow.prototype._adjustWindow = function (vector) {
+		var direction = motionutils.calculateDirection(vector, vector.timestamp);
+		var adjust = false;		
+		if (this._low === undefined || this._high === undefined) {
+			adjust = true;
+		} else if (vector.position < this._low || this._high < vector.position) {
+			adjust = true;
+		} else if (vector.position === this._low && direction === -1) {
+			adjust = true;
+		} else if (vector.position === this._high && direction === 1) {
+			adjust = true;
+		}
+		if (adjust) {	
+			// simple solution - just center
+			var quarter = this._length/4.0;
+			this._low = vector.position - quarter;
+			this._high = vector.position + quarter;
+			return true;
+		} else return false;
 	};
 
-	SlidingWindow.prototype._toHandler = function () {
-		console.log("change");
+	SlidingWindow.prototype._calculateTimeoutVector = function () {
+		var freshVector = this._to.query();
+		var res = motionutils.calculateDelta(freshVector, [this._low, this._high]);
+		var deltaSec = res[0];
+		if (deltaSec === null) return null;
+		var position = res[1];
+		var vector = motionutils.calculateVector(freshVector, freshVector.timestamp + deltaSec);
+		vector.position = position; // avoid rounding errors
+		return vector;
+	};
 
+	SlidingWindow.prototype._clearTimeout = function () {
+		if (this._timeout !== null) {
+			this._timeout.cancel();
+			this._timeout = null;
+		}
+	};
+
+	SlidingWindow.prototype._adjustTimeout = function () {
+		this._clearTimeout();
+		var vector = this._calculateTimeoutVector();
+		if (vector === null) {return;}	 		
+		var now = this._to.clock.now();
+ 		var secDelay = vector.timestamp - now;
+ 		var self = this;
+ 		this._timeout = this._to.clock.setTimeout(function () {
+			self._onTimeout(vector);
+      	}, secDelay, {anchor: now, early: 0.005}); 	
+	};
+
+	SlidingWindow.prototype._onTimeout = function (vector) {
+		this._process(vector);
+	};
+
+	SlidingWindow.prototype._onChange = function () {
+		this._process(this._to.query());
+	};
+
+	SlidingWindow.prototype._process = function (vector) {
 		// adjust window if necessary
-		var vector = this._to.query();
-
-		// logic should be similar to rangeconverter
-
+		var changed = this._adjustWindow(vector);
+		// adjust timeout
+		var vector = this._adjustTimeout();
+		// notify window change
+		if (changed) {
+			this.eventifyTriggerEvent("change");	
+		}
 	};
 
 	return SlidingWindow;
