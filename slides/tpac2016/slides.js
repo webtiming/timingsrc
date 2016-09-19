b7c0c5992b60d07db1588bd775434780d73b1cf3
Ingar Arntzen <ingar.arntzen@gmail.com>
2016-09-19 18:59:53 +0200

    integrated shower js with timing object

---
+++ slides/tpac2016/slides.js
@@ -0,0 +1,222 @@
+var slides = [
+  {
+    id : "all",
+    start: 0,
+    end: 1
+  },
+  {
+    id : "Cover",
+    start: 1,
+    end: 2
+  },
+  {
+    id : "outline",
+    start: 2,
+    end: 3
+  },
+  {
+    id : "part1",
+    start: 3,
+    end: 4
+  },
+  {
+    id : "mtcg",
+    start: 4,
+    end: 5
+  },
+  {
+    id : "status-cg",
+    start: 5,
+    end: 6
+  },
+  {
+    id : "status-media",
+    start: 6,
+    end: 7
+  },
+  {
+    id : "definition",
+    start: 7,
+    end: 8
+  },
+  {
+    id : "challenges",
+    start: 8,
+    end: 9
+  },
+  {
+    id : "realities-page-internal",
+    start: 9,
+    end: 10
+  },
+  {
+    id : "realities-cross-page",
+    start: 10,
+    end: 11
+  },
+  {
+    id : "goals",
+    start: 11,
+    end: 12
+  },
+  {
+    id : "easy-peasy",
+    start: 12,
+    end: 13
+  },
+  {
+    id : "part2",
+    start: 13,
+    end: 14
+  },
+  {
+    id : "timing-object",
+    start: 14,
+    end: 15
+  },
+  {
+    id : "timing-object-page",
+    start: 15,
+    end: 16
+  },
+  {
+    id : "timing-object-online",
+    start: 16,
+    end: 17
+  },
+  {
+    id : "approach",
+    start: 17,
+    end: 18
+  },
+  {
+    id : "applications-table",
+    start: 18,
+    end: 19
+  },
+  {
+    id : "social-viewing",
+    start: 19,
+    end: 20
+  },
+  {
+    id : "multi-screen",
+    start: 20,
+    end: 21
+  },
+  {
+    id : "multi-device",
+    start: 21,
+    end: 22
+  },
+  {
+  id : "remote-control",
+    start: 22,
+    end: 23
+  },
+  {
+    id : "music",
+    start: 23,
+    end: 24
+  },
+  {
+    id : "split",
+    start: 24,
+    end: 25
+  },
+  {
+    id : "capture",
+    start: 25,
+    end: 26
+  },
+  {
+    id : "integration",
+    start: 26,
+    end: 27
+  },
+  {
+    id : "part4",
+    start: 27,
+    end: 28
+  },
+  {
+    id : "android-video",
+    start: 28,
+    end: 29
+  },
+  {
+    id : "android-audio",
+    start: 29,
+    end: 30
+  },
+  {
+    id : "chrome-video",
+    start: 30,
+    end: 31
+  },
+  {
+    id : "chrome-audio",
+    start: 31,
+    end: 32
+  },
+  {
+    id : "firefox-video",
+    start: 32,
+    end: 33
+  },
+  {
+    id : "firefox-audio",
+    start: 33,
+    end: 34
+  },
+  {
+    id : "ipad-video",
+    start: 34,
+    end: 35
+  },
+  {
+    id : "ipad-audio",
+    start: 35,
+    end: 36
+  },
+  {
+    id : "ipad-video-skip",
+    start: 36,
+    end: 37
+  },
+  {
+    id : "ipad-audio-skip",
+    start: 37,
+    end: 38
+  },
+  {
+    id : "part5",
+    start: 38,
+    end: 39
+  },
+  {
+    id : "standardization",
+    start: 39,
+    end: 40
+  },
+  {
+    id : "initiatives",
+    start: 40,
+    end: 41
+  },
+  {
+    id : "nextsteps",
+    start: 41,
+    end: 42
+  },
+  {
+    id : "summary",
+    start: 42,
+    end: 43
+  },
+  {
+    id : "Conver2",
+    start: 43,
+    end: 44
+  }
+];
---
+++ slides/tpac2016/to-controls.js
@@ -0,0 +1,138 @@
+
+var TM = function(_TMC_) {
+
+
+
+  var parseNumber = function(n) {
+    var f = parseFloat(n);
+    if (!isNaN(f) && isFinite(n)) {
+      return f;
+    };
+    return null;
+  };
+
+  var toInput = function (elemId, to) {
+      var rootElem = document.getElementById(elemId);
+
+      var html = "";
+      html += '<div class="toinput">'
+      html += 'Update&nbsp;(&nbsp;<input id="p" type="text" name="p" value="" size="4">,';
+      html += '&nbsp;<input id="v" type="text" name="v" value="" size="4">,';
+      html += '&nbsp;<input id="a" type="text" name="a" value="" size="4">&nbsp;)';
+      html += '&nbsp;<input id="submit" type="submit" value="Update">';
+      html += '</div>'
+      rootElem.innerHTML = html;
+
+      // find elements
+      var pElem = rootElem.querySelector("#p");
+      var vElem = rootElem.querySelector("#v");
+      var aElem = rootElem.querySelector("#a");
+      var btnElem = rootElem.querySelector("#submit");
+
+      btnElem.onclick = function (e) {
+        e.preventDefault();
+
+        // Check input values
+        var p = parseNumber(pElem.value);
+        var v = parseNumber(vElem.value);
+        var a = parseNumber(aElem.value);
+
+        var vector = {};
+        if (p !== null) vector.position = p;
+        if (v !== null) vector.velocity = v;
+        if (a !== null) vector.acceleration = a;
+
+        // Clear input values
+        pElem.value = "";
+        vElem.value = "";
+        aElem.value = "";
+
+        // Update
+        if (p !== null || v !== null || a !== null) {
+          to.update(vector);
+        }
+       
+        return false;
+      };
+  };
+
+
+
+  var toValue = function(elemId, to) {
+    var rootElem = document.getElementById(elemId);
+    rootElem.innerHTML = '<div id="tovalue" class="tovalue"></div>';
+    var valueElem = rootElem.querySelector("#tovalue");
+
+    var refresh = function (e) {
+      var p, v, a;
+      var vector = to.query();
+      p = vector.position.toFixed(1),
+      v = vector.velocity.toFixed(1);
+      a = vector.acceleration.toFixed(1);
+      valueElem.innerHTML = "P:" + p + "  V:" + v + "  A:" + a;
+    };
+    // initialise
+    to.on("timeupdate", refresh);
+  };
+
+  var toControl = function (elemId, to) {
+    var rootElem = document.getElementById(elemId);
+
+    var html = '<div class="tocontrol">';
+    html += '<button id="p_down">P--</button>';
+    html += '<button id="p_zero">P=0</button>';
+    html += '<button id="p_up">P++</button>';
+    html += '<button id="v_down">V--</button>';
+    html += '<button id="v_zero">V=0</button>';
+    html += '<button id="v_up">V++</button>';
+    html += '<button id="a_down">A--</button>';
+    html += '<button id="a_zero">A=0</button>';
+    html += '<button id="a_up">A++</button>';
+    html += '</div>';
+    rootElem.innerHTML = html;
+
+    // Hook up buttons UI
+    rootElem.onclick = function (e) {  
+      var vector = to.query();
+      var p = vector.position;
+      var v = vector.velocity;
+      var a = vector.acceleration;
+      switch (e.target.id) {
+        case "p_down":
+          p = p-1.0;
+          break;
+        case  "p_up":
+          p = p+1.0;
+          break;
+        case "p_zero":
+          p = 0.0;
+          break;
+        case "v_down":
+          v = v-1.0;
+          break;
+        case  "v_up":
+          v = v+1.0;
+          break;
+        case "v_zero":
+          v = 0.0;
+          break;
+        case "a_down":
+          a = a-1.0;
+          break;
+        case  "a_up":
+          a = a+1.0;
+          break;
+        case "a_zero":
+          a = 0.0;
+          break;
+      }
+      to.update({position:p, velocity:v, acceleration:a});
+    };
+  };
+
+  _TMC_.toControl = toControl;
+  _TMC_.toValue = toValue;
+  _TMC_.toInput = toInput;
+  return _TMC_;
+
+}(TM || {});
