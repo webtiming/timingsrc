14ea7958d1f941fde6496459d6dd23d7a9eeebdd
Ingar Arntzen <ingar.arntzen@gmail.com>
2016-11-16 23:07:20 +0100

    added get and items to sequencer api

--- v2/sequencing/sequencer.js
+++ v2/sequencing/sequencer.js
@@ -1198,11 +1198,19 @@ define(['util/motionutils', 'util/eventify', 'util/interval', './axis'],
 		return this._axis.keys();
 	};
 	
+	Sequencer.prototype.get = function (key) {
+		return this.getCues(key);
+	};
+
 	// get specific cue {key: key, interval:interva} given key
 	Sequencer.prototype.getCue = function (key) {
 		if (this._axis.hasKey(key)) {
 			return new SequencerCue (this._axis.getItem(key));
 		}  
+	};
+
+	Sequencer.prototype.items = function () {
+		return this.getCues();
 	};
 
 	// get all cues
