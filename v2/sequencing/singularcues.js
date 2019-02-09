0a1c9abdc1196e88aca5c57390b938d9d4049ffe
Ingar Arntzen <ingar.arntzen@gmail.com>
2019-02-09 19:42:08 +0100

    make makePointMapIterable a function and switched to CamelCase naming

--- v2/sequencing/singularcues.js
+++ v2/sequencing/singularcues.js
@@ -37,6 +37,52 @@ define (['../util/binarysearch'],
 		return arr.length == 0;
 	};
 
+	/*
+		make iterable for pointMap
+	
+		pointIterable is an iterable of points
+		pointMapIterable will iterate the points and find
+		the associated cues from pointMap
+
+		pointMapIterable returns values {point: point, cue:cue}
+		where cue is associated with point
+	*/
+	var makePointMapIterable = function (pointMap, pointIterable) {
+		let pointIterator = pointIterable[Symbol.iterator]();
+		let cueIterator = [].values();
+		let pointItem;
+		let next = function () {
+			let cueItem = cueIterator.next();
+			if (cueItem.done) {
+				// fetch new cues from pointIterable
+				pointItem = pointIterator.next();
+				if (pointItem.done) {
+					// both exhausted
+					return {done: true};
+				} else {
+					// more cues
+					cueIterator = pointMap.get(pointItem.value).values();
+					cueItem = cueIterator.next();
+					// newly fetched cueIterator should never be empty
+					if (cueItem.done) {
+						throw new Error("no cues in pointMap for point ", pointItem.value);
+					}	
+				}
+			} 
+			return {
+				done:false,
+				value: {
+					point:pointItem.value, 
+					cue: cueItem.value
+				}
+			}
+		};
+		// return iterable
+		return {
+			next: next,
+			[Symbol.iterator]: function () {return this;}
+		};
+	};
 
 
 
@@ -53,28 +99,28 @@ define (['../util/binarysearch'],
 			efficient lookup of cues by key	
 			key -> cue
 		*/
-		this.keymap = new Map();
+		this.keyMap = new Map();
 
 		/*
-			pointmap maintains the associations between values (points on 
+			pointMap maintains the associations between values (points on 
 			the timeline) and cues that reference such points. A single point value may be 
 			referenced by multiple cues, so one point value maps to a list of cues.
 	
 			value -> [cue, ....]
 		*/ 
-		this.pointmap = new Map(); 
+		this.pointMap = new Map(); 
 
 		/*
-			pointindex maintains a sorted list of numbers for efficient lookup.
+			pointIndex maintains a sorted list of numbers for efficient lookup.
 			A large volume of insert and remove operations may be problematic
 			with respect to performance, so the implementation seeks to
 			do a single bulk update on this structure, for each batch of cue
 			operations (i.e. each invocations of addCues). In order to do this 
 			all cue operations are processed to calculate a single batch 
 			of deletes and a single batch of inserts which then will be applied to 
-			the pointindex in one atomic operation. 
+			the pointIndex in one atomic operation. 
 		*/
-		this.pointindex = new BinarySearch();
+		this.pointIndex = new BinarySearch();
 	};
 
 	/*
@@ -99,42 +145,42 @@ define (['../util/binarysearch'],
 	Axis.prototype.update = function(cues) {
      	if (cues.length == 0) { return [];}
 	
-		let cueBatch = new CueBatch(this.pointmap, this.pointindex);
+		let cueBatch = new CueBatch(this.pointMap, this.pointIndex);
 		let eventBatch = new EventBatch();
 
     	let cue, old_cue;
-        if (this.keymap.size == 0) {
+        if (this.keyMap.size == 0) {
         	// initialization - first invocation of addCues
         	for (let i=0; i<cues.length; i++) {
         		cue = cues[i];
-        		this.keymap.set(cue.key, cue);
+        		this.keyMap.set(cue.key, cue);
         		cueBatch.processCue("add", cue.interval.low, cue);
 				eventBatch.processEvent({new:cue});
         	}
         } else {
-        	// iterate keys in keymap
+        	// iterate keys in keyMap
         	for (let i=0; i<cues.length; i++) {
 				// check for cues to be replaced
 				cue = cues[i];
-				old_cue = this.keymap.get(cue.key);
+				old_cue = this.keyMap.get(cue.key);
 				if (old_cue) {
 					if (cue.interval) {
 						// replace cue
-						this.keymap.set(cue.key, cue);
+						this.keyMap.set(cue.key, cue);
 						cueBatch.processCue("remove", old_cue.interval.low, old_cue);
 						cueBatch.processCue("add", cue.interval.low, cue);
 						eventBatch.processEvent({new:cue, old:old_cue});
 					} else {
 						// delete cue
 						// cues without interval signify deletion 
-						this.keymap.delete(old_cue.key);
+						this.keyMap.delete(old_cue.key);
 						cueBatch.processCue("remove", old_cue.interval.low, old_cue);
 						eventBatch.processEvent({old:old_cue});
 					}
 				} else {
 					if (cue.interval) {
 						// new cue
-						this.keymap.set(cue.key, cue);
+						this.keyMap.set(cue.key, cue);
 						cueBatch.processCue("add", cue.interval.low, cue);
 						eventBatch.processEvent({new:cue});
 					} else {
@@ -147,7 +193,7 @@ define (['../util/binarysearch'],
 
         /*
 			finalizing cueBatch causes modification to be committed
-			to pointmap and pointindex
+			to pointMap and pointIndex
 			
 			finalizing eventBatch returns a list of events
         */
@@ -167,41 +213,8 @@ define (['../util/binarysearch'],
 		Find all interval endpoints within given interval 
 	*/
 	Axis.prototype.getPointsCoveredByInterval = function (interval) {
-		let pointIterator = this.pointindex.lookup(interval)[Symbol.iterator]();
-		let cueIterator = [].values();
-		let self = this;
-		let pointItem;
-		let next = function () {
-			let cueItem = cueIterator.next();
-			if (cueItem.done) {
-				// fetch new cues from pointIterable
-				pointItem = pointIterator.next();
-				if (pointItem.done) {
-					// both exhausted
-					return {done: true};
-				} else {
-					// more cues
-					cueIterator = self.pointmap.get(pointItem.value).values();
-					cueItem = cueIterator.next();
-					// newly fetched cueIterator should never be empty
-					if (cueItem.done) {
-						throw new Error("empty node in pointmap");
-					}	
-				}
-			} 
-			return {
-				done:false,
-				value: {
-					point:pointItem.value, 
-					cue: cueItem.value
-				}
-			}
-		};
-		// return iterable
-		return {
-			next: next,
-			[Symbol.iterator]: function () {return this;}
-		};
+		let pointIterable = this.pointIndex.lookup(interval);
+		return makePointMapIterable(this.pointMap, pointIterable);
 	};
 
 
@@ -220,7 +233,7 @@ define (['../util/binarysearch'],
 
 		Axis update() processes a batch of cues, and needs to 
 		translates cue operations into a minimum set of 
-		operations on the pointindex.
+		operations on the pointIndex.
 
 		To do this, we need to records points that are created and
 		points which are modified.
@@ -228,32 +241,32 @@ define (['../util/binarysearch'],
 		The total difference that the batch of cue operations
 		amounts to is expressed as one list of values to be 
 		deleted, and and one to be inserted. The update operation
-		of the pointindex will process both in one atomic
+		of the pointIndex will process both in one atomic
 		operation.
 
-		On submit both the pointmap and the pointindex will brought
+		On submit both the pointMap and the pointIndex will brought
 		up to speed
 
 		created and dirty are used for bookeeping during 
 		processing of a cue batch. They are needed to 
-		create the correct diff operation to be applied on pointindex.
+		create the correct diff operation to be applied on pointIndex.
 
-		created : includes nodes that were not in pointmap 
+		created : includes nodes that were not in pointMap 
 		before current batch was processed
 
-		dirty : includes nodes that were in pointmap
+		dirty : includes nodes that were in pointMap
 		before curent batch was processed, and that
 		have been become empty at least at one point during cue 
 		processing. 
 
-		created and dirty are used as temporary alternatives to pointmap.
+		created and dirty are used as temporary alternatives to pointMap.
 		after the cue processing, poinmap will updated based on the
 		contents of these to  
 	*/
 
-	var CueBatch = function (pointmap, pointindex) {
-		this.pointmap = pointmap;
-		this.pointindex = pointindex;
+	var CueBatch = function (pointMap, pointIndex) {
+		this.pointMap = pointMap;
+		this.pointIndex = pointIndex;
 		this.created = new Map(); // value -> [cue, ...]
 		this.dirty = new Map(); // value -> [cue, ...]
 	};
@@ -268,14 +281,14 @@ define (['../util/binarysearch'],
 		let cues = this.created.get(point);
 		if (cues == undefined) {
 			// point not found in created
-			cues = this.pointmap.get(point);
+			cues = this.pointMap.get(point);
 			if (cues == undefined) {
-				// point not found in pointmap
+				// point not found in pointMap
 				// register in created
 				cues = (op == "add") ? [cue] : [];
 				this.created.set(point, cues);
 			} else {
-				// cues found in pointmap - update
+				// cues found in pointMap - update
 				if (op == "add") {
 					addCueToArray(cues, cue);
 				} else {
@@ -298,33 +311,33 @@ define (['../util/binarysearch'],
 
 	/*
 		Batch processing is completed
-		Commit changes to pointindex and pointmap.
+		Commit changes to pointIndex and pointMap.
 
-		pointmap
+		pointMap
 		- update with contents of created
 
-		pointindex
+		pointIndex
 		- points to delete - dirty and empty
 		- points to insert - created and non-empty
 	*/
 	CueBatch.prototype.done = function () {
-		// update pointmap and pointindex
+		// update pointMap and pointIndex
 		let value, node;
 		let to_remove = [];
 		let to_insert = [];
 		for ([value, node] of this.created.entries()) {
 			if (node.length > 0) {
 				to_insert.push(value);
-				this.pointmap.set(value, node);
+				this.pointMap.set(value, node);
 			} 
 		}
 		for ([value, node] of this.dirty.entries()) {
 			if (node.length == 0) {
 				to_remove.push(value);
-				this.pointmap.delete(value);
+				this.pointMap.delete(value);
 			}
 		}
-		this.pointindex.update(to_remove, to_insert);
+		this.pointIndex.update(to_remove, to_insert);
 		// cleanup
 		this.created.clear();
 		this.dirty.clear();
