262735045a24b98ff19c3ef69cb75c2540bb724e
Ingar <ingar.arntzen@norut.no>
2019-03-10 22:35:34 +0100

    made build script for v2.1

---
+++ build/almond-build-v2.1.js
@@ -0,0 +1,10 @@
+{
+    baseUrl: '../v2.1',
+    name: '../build/almond',
+    include: ['timingsrc'],
+    //insertRequire: ['timingsrc'],
+    wrap: {
+        startFile: 'start.frag',
+        endFile: 'end.frag'
+    }
+}
--- build/build.sh
+++ build/build.sh
@@ -7,3 +7,8 @@ node r.js -o baseUrl=../v2 name=timingsrc out=../lib/timingsrc-require-min-v2.js
 node r.js -o baseUrl=../v2 name=timingsrc optimize=none out=../lib/timingsrc-require-v2.js # built for requirejs usage
 node r.js -o almond-build-v2.js optimize=none out=../lib/timingsrc-v2.js # built for regular script includes
 node r.js -o almond-build-v2.js out=../lib/timingsrc-min-v2.js # built and minified for regular script includes
+
+node r.js -o baseUrl=../v2.1 name=timingsrc out=../lib/timingsrc-require-min-v2.1.js # built and minified for requirejs usage
+node r.js -o baseUrl=../v2.1 name=timingsrc optimize=none out=../lib/timingsrc-require-v2.1.js # built for requirejs usage
+node r.js -o almond-build-v2.1.js optimize=none out=../lib/timingsrc-v2.1.js # built for regular script includes
+node r.js -o almond-build-v2.1.js out=../lib/timingsrc-min-v2.1.js # built and minified for regular script includes
