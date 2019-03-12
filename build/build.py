a38aabb00451c87f2464c04017706e35617b21fe
Ingar <ingar.arntzen@norut.no>
2019-03-12 14:49:35 +0100

    make python based build script

---
+++ build/build.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import subprocess
+
+versions = ["v1", "v2", "v2.1"]
+
+def build (version):
+	srcdir = os.path.join("../", version)
+	libdir = "../lib"
+
+	baseUrl = "baseUrl={}".format(srcdir)
+	almond = "almond-build-{}.js".format(version)
+
+	# requirejs - unminified		
+	out = "out={}/timingsrc-require-{}.js".format(libdir, version)
+	args = ["node", "r.js", "-o", baseUrl, "optimize=none", "name=timingsrc", out]
+	print(" ".join(args))
+	subprocess.call(args)
+
+	# requirejs - minified
+	out = "out={}/timingsrc-min-require-{}.js".format(libdir, version)
+	args = ["node", "r.js", "-o", baseUrl, "name=timingsrc", out]
+	print(" ".join(args))
+	subprocess.call(args)
+
+	# plain - unminified
+	out = "out={}/timingsrc-{}.js".format(libdir, version)
+	args = ["node", "r.js", "-o", almond, "optimize=none", out]
+	print(" ".join(args))
+	subprocess.call(args)
+
+	# plain - minified
+	out = "out={}/timingsrc-min-{}.js".format(libdir, version)
+	args = ["node", "r.js", "-o", almond, out]
+	print(" ".join(args))
+	subprocess.call(args)
+
+		
+if __name__ == '__main__':
+
+	if len(sys.argv) > 1:
+		_versions = [sys.argv[1]]
+	else:
+		_versions = versions
+
+	for version in _versions:
+		build(version)
+
+
