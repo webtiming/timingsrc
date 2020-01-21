835233ee9e0ec6c54903d55d7991ff4cff536df9
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-01-21 23:22:26 +0100

    integrating cmp with Interval

--- v2.1/util/interval.js
+++ v2.1/util/interval.js
@@ -22,250 +22,20 @@ define(function () {
 
 	'use strict';
 
-	/*
-		INTERVAL
-	*/
 
-	var isNumber = function(n) {
-		var N = parseFloat(n);
+	const isNumber = function(n) {
+		let N = parseFloat(n);
 	    return (n===N && !isNaN(N));
 	};
 
-	var IntervalError = function (message) {
-		this.name = "IntervalError";
-		this.message = (message||"");
-	};
-	IntervalError.prototype = Error.prototype;
 
+	/*********************************************************
 
-	class Interval {
+	ENDPOINT
 
-		constructor (low, high, lowInclude, highInclude) {
-			if (!(this instanceof Interval)) {
-				throw new Error("Contructor function called without new operation");
-			}
-			var lowIsNumber = isNumber(low);
-			var highIsNumber = isNumber(high);
-			// new Interval(3.0) defines singular - low === high
-			if (lowIsNumber && high === undefined) high = low;
-			if (!isNumber(low)) throw new IntervalError("low not a number");
-			if (!isNumber(high)) throw new IntervalError("high not a number");
-			if (low > high) throw new IntervalError("low > high");
-			if (low === high) {
-				lowInclude = true;
-				highInclude = true;
-			}
-			if (low === -Infinity) lowInclude = true;
-			if (high === Infinity) highInclude = true;
-			if (lowInclude === undefined) lowInclude = true;
-			if (highInclude === undefined) highInclude = false;
-			if (typeof lowInclude !== "boolean") throw new IntervalError("lowInclude not boolean");
-			if (typeof highInclude !== "boolean") throw new IntervalError("highInclude not boolean");
-			this.low = low;
-			this.high = high;
-			this.lowInclude = lowInclude;
-			this.highInclude = highInclude;
-			this.length = this.high - this.low;
-			this.singular = (this.low === this.high);
-			this.finite = (isFinite(this.low) && isFinite(this.high));
-		}
+	Utilities for interval endpoints comparisons
 
-
-
-		toString () {
-			var lowBracket = (this.lowInclude) ? "[" : "<";
-			var highBracket = (this.highInclude) ? "]" : ">";
-			var low = (this.low === -Infinity) ? "<--" : this.low; //.toFixed(2);
-			var high = (this.high === Infinity) ? "-->" : this.high; //.toFixed(2);
-			if (this.singular)
-				return lowBracket + low + highBracket;
-			return lowBracket + low + ',' + high + highBracket;
-		};
-
-		coversPoint (x) {
-			if (this.low < x && x < this.high) return true;
-			if (this.lowInclude && x === this.low) return true;
-			if (this.highInclude && x === this.high) return true;
-			return false;
-		};
-
-		// overlap : it exists at least one point x covered by both interval
-		overlapsInterval(other) {
-			if (other instanceof Interval === false) throw new IntervalError("paramenter not instance of Interval");
-			// singularities
-			if (this.singular && other.singular)
-				return (this.low === other.low);
-			if (this.singular)
-				return other.coversPoint(this.low);
-			if (other.singular)
-				return this.coversPoint(other.low);
-			// not overlap right
-			if (this.high < other.low) return false;
-			if (this.high === other.low) {
-				return this.coversPoint(other.low) && other.coversPoint(this.high);
-			}
-			// not overlap left
-			if (this.low > other.high) return false;
-			if (this.low === other.high) {
-				return (this.coversPoint(other.high) && other.coversPoint(this.low));
-			}
-			return true;
-		};
-
-		// Interval fully covering other interval
-		coversInterval (other) {
-			if (other instanceof Interval === false) throw new IntervalError("paramenter not instance of Interval");
-			if (other.low < this.low || this.high < other.high) return false;
-			if (this.low < other.low && other.high < this.high) return true;
-			// corner case - one or both endpoints are the same (the other endpoint is covered)
-			if (this.low === other.low && this.lowInclude === false && other.lowInclude === true)
-				return false;
-			if (this.high === other.high && this.highInclude === false && other.highInclude === true)
-				return false;
-			return true;
-		};
-		equals (other) {
-			if (this.low !== other.low) return false;
-			if (this.high !== other.high) return false;
-			if (this.lowInclude !== other.lowInclude) return false;
-			if (this.highInclude !== other.highInclude) return false;
-			return true;
-		};
-
-
-
-
-
-
-
-
-
-
-
-
-		/*
-			a.hasEndpointInside(b)
-
-			returns true if interval a has at least one endpoint inside interval b
-
-			This is easy for most intervals, but there are some subtleties
-			when when interval a and b have one or two endpoints in common
-
-			4 ways for intervals to share an endpoint
-
-			- a.high == b.low :
-				><  (a.high outside b)
-				>[  (a.high outside b)
-				]<  (a.high outside b)
-				][  (a.high inside b)
-			- a.high == b.high:
-				>>  (a.high inside b)
-				>]  (a.high inside b)
-				]>  (a.high outside b)
-				]]  (a.high inside b)
-			- a.low == b.low :
-				<<  (a.low inside b)
-				<[  (a.low inside b)
-				[<  (a.low outside b)
-				[[  (a.low inside b)
-			- a.low == b.high :
-				<>  (a.low outside b)
-				[>  (a.low outside b)
-				<]  (a.low outside b)
-				[]  (a.low inside b)
-
-		*/
-
-
-		hasEndpointInside (b) {
-			const a = this;
-			// check if a is to the right of b
-			if (b.high < a.low) return false;
-			// check if a is to the left of b
-			if (a.high < b.low) return false;
-			// check if a.low is inside b
-			if (b.low < a.low && a.low < b.high) return true;
-			// check if a.high is inside b
-			if (b.low < a.high && a.high < b.high) return true;
-
-			// special consideration if a and b share endpoint(s)
-
-			// a.high shared
-			if (a.high == b.low) {
-				if (a.highInclude && b.lowInclude) return true;
-			}
-			if (a.high == b.high) {
-				if (!(a.highInclude && !b.highInclude)) return true;
-			}
-			// a.low shared
-			if (a.low == b.low) {
-				if (!(a.lowInclude && !b.lowInclude)) return true;
-			}
-			if (a.low == b.high) {
-				if (a.lowInclude && b.highInclude) return true;
-			}
-			return false;
-		};
-
-	}
-
-	function _make_interval_cmp(endpoint) {
-		/*
-			make sort function for sorting intervals based on
-			endpoint "low" or "high" in ascending
-
-			if inteval_a and interval_b have same endpoint value
-			compare lowInclude, highInclude
-
-			low : AB
-				  [[ - equal   - return 0
-				  [< - A first - return 1
-				  <[ - B first - return -1
-				  << - equal   - return 0
-
-			high : AB
-				  ]] - equal   - return 0
-				  ]> - B first - return -1
-				  >] - A first - return 1
-				  >> - equal   - return 0
-
-
-			returns cmp function to be used with array.sort()
-		*/
-
-		function cmp (interval_a, interval_b) {
-			// sort first by value
-			let diff = interval_b[endpoint] - interval_a[endpoint]
-			if (diff == 0) {
-				// equal endpoint values - compare endpoint include
-				let a_include = interval_a[endpoint+"Include"];
-				let b_include = interval_b[endpoint+"Include"];
-
-				if (a_include == b_include) {
-					return 0;
-				}
-				if (endpoint == "low") {
-					return (a_include) ? 1 : -1
-				} else {
-					return (a_include) ? -1 : 1
-				}
-			} else {
-				return diff;
-			}
-		}
-		return cmp;
-	}
-
-	const cmp_interval_low = _make_interval_cmp("low");
-	const cmp_interval_high = _make_interval_cmp("high");
-
-
-
-	/*
-
-		Internal endpoint functionality
-
-	*/
+	**********************************************************/
 
 	const endpoint = function () {
 
@@ -293,14 +63,14 @@ define(function () {
 		order[MODE_LEFT_OPEN] = 4;
 
 		/*
-			order
+			get order
 
 			given two endpoints return
 			two numbers representing their order
 			also accepts regular numbers as endpoints
 
-			for points values/endpoint values that are not
-			equal, these values convey order,
+			for points or endpoint values that are not
+			equal, these values convey order directly,
 			otherwise the order numbers 0-4 are returned
 			based on endpoint inclusion and direction
 
@@ -426,51 +196,54 @@ define(function () {
 	}();
 
 
-	/*
-		inside (e, interval)
+	/*********************************************************
+	INSIDE INTERVAL
+	**********************************************************
 
-		figure out if given endpoint/point e is inside interval
-		returns bool
-	*/
+	inside (e, interval)
+
+	return true if given point e is inside interval
+
+	**********************************************************/
 
 	function inside (e, interval) {
-		if (! interval instanceof Interval) {
-			throw new Error("not interval", interval);
-		}
 		let e_low = [interval.low, false, interval.lowInclude];
 		let e_high = [interval.high, true, interval.highInclude];
 		return !endpoint.leftof(e, e_low) && !endpoint.rightof(e, e_high);
 	}
 
-	/*
-		compare (a, b)
-		param a Interval
-		param b Interval
-		returns IntervalRelation
 
-		compares interval b to interval a
-		e.g. return value COVERED reads b is covered by a.
+	/*********************************************************
+	COMPARE INTERVALS
+	**********************************************************
 
+	compare (a, b)
+	param a Interval
+	param b Interval
+	returns IntervalRelation
 
-		cmp_1 = endpoint_compare(b_low, a_low);
-		cmp_2 = endpoint_compare(b_high, a_high);
+	compares interval b to interval a
+	e.g. return value COVERED reads b is covered by a.
 
-		key = 10*cmp_1 + cmp_2
+	cmp_1 = endpoint_compare(b_low, a_low);
+	cmp_2 = endpoint_compare(b_high, a_high);
 
-		cmp_1  cmp_2  key  relation
-		=====  =====  ===  ============================
-		1 	   1       11  OUTSIDE_LEFT, PARTIAL_LEFT
-		1 	   0       10  COVERS
-		1      -1       9  COVERS
-		0 	   1        1  COVERED
-		0      0        0  EQUAL
-		0	   -1      -1  COVERS
-		-1     1       -9  COVERED
-		-1 	   0      -10  COVERED
-		-1     -1     -11  OUTSIDE_RIGHT, PARTIAL_RIGHT
-		=====  =====  ===  ============================
+	key = 10*cmp_1 + cmp_2
 
-	*/
+	cmp_1  cmp_2  key  relation
+	=====  =====  ===  ============================
+	1 	   1       11  OUTSIDE_LEFT, PARTIAL_LEFT
+	1 	   0       10  COVERS
+	1      -1       9  COVERS
+	0 	   1        1  COVERED
+	0      0        0  EQUAL
+	0	   -1      -1  COVERS
+	-1     1       -9  COVERED
+	-1 	   0      -10  COVERED
+	-1     -1     -11  OUTSIDE_RIGHT, PARTIAL_RIGHT
+	=====  =====  ===  ============================
+	
+	**********************************************************/
 
     const IntervalRelation = Object.freeze({
 		COVERED: 1,
@@ -496,7 +269,7 @@ define(function () {
 			if (isNumber(b)) {
 				b = new Interval(b);
 			} else {
-				throw new Error("b not interval", a);
+				throw new Error("b not interval", b);
 			}
 		}
 		// interval endpoints
@@ -533,16 +306,262 @@ define(function () {
 		}
 	}
 
+
+
+	/*********************************************************
+	INTERVAL ERROR
+	**********************************************************/
+
+	var IntervalError = function (message) {
+		this.name = "IntervalError";
+		this.message = (message||"");
+	};
+	IntervalError.prototype = Error.prototype;
+
+
+	/*********************************************************
+	INTERVAL
+	**********************************************************/
+
+	class Interval {
+
+		constructor (low, high, lowInclude, highInclude) {
+			if (!(this instanceof Interval)) {
+				throw new Error("Contructor function called without new operation");
+			}
+			var lowIsNumber = isNumber(low);
+			var highIsNumber = isNumber(high);
+			// new Interval(3.0) defines singular - low === high
+			if (lowIsNumber && high === undefined) high = low;
+			if (!isNumber(low)) throw new IntervalError("low not a number");
+			if (!isNumber(high)) throw new IntervalError("high not a number");
+			if (low > high) throw new IntervalError("low > high");
+			if (low === high) {
+				lowInclude = true;
+				highInclude = true;
+			}
+			if (low === -Infinity) lowInclude = true;
+			if (high === Infinity) highInclude = true;
+			if (lowInclude === undefined) lowInclude = true;
+			if (highInclude === undefined) highInclude = false;
+			if (typeof lowInclude !== "boolean") throw new IntervalError("lowInclude not boolean");
+			if (typeof highInclude !== "boolean") throw new IntervalError("highInclude not boolean");
+			this.low = low;
+			this.high = high;
+			this.lowInclude = lowInclude;
+			this.highInclude = highInclude;
+			this.length = this.high - this.low;
+			this.singular = (this.low === this.high);
+			this.finite = (isFinite(this.low) && isFinite(this.high));
+		}
+
+		toString () {
+			var lowBracket = (this.lowInclude) ? "[" : "<";
+			var highBracket = (this.highInclude) ? "]" : ">";
+			var low = (this.low === -Infinity) ? "<--" : this.low; //.toFixed(2);
+			var high = (this.high === Infinity) ? "-->" : this.high; //.toFixed(2);
+			if (this.singular)
+				return lowBracket + low + highBracket;
+			return lowBracket + low + ',' + high + highBracket;
+		};
+
+
+		/*
+		coversPoint (x) {
+			if (this.low < x && x < this.high) return true;
+			if (this.lowInclude && x === this.low) return true;
+			if (this.highInclude && x === this.high) return true;
+			return false;
+		};
+
+		// overlap : it exists at least one point x covered by both interval
+		overlapsInterval(other) {
+			if (other instanceof Interval === false) throw new IntervalError("paramenter not instance of Interval");
+			// singularities
+			if (this.singular && other.singular)
+				return (this.low === other.low);
+			if (this.singular)
+				return other.coversPoint(this.low);
+			if (other.singular)
+				return this.coversPoint(other.low);
+			// not overlap right
+			if (this.high < other.low) return false;
+			if (this.high === other.low) {
+				return this.coversPoint(other.low) && other.coversPoint(this.high);
+			}
+			// not overlap left
+			if (this.low > other.high) return false;
+			if (this.low === other.high) {
+				return (this.coversPoint(other.high) && other.coversPoint(this.low));
+			}
+			return true;
+		};
+
+		// Interval fully covering other interval
+		coversInterval (other) {
+			if (other instanceof Interval === false) throw new IntervalError("paramenter not instance of Interval");
+			if (other.low < this.low || this.high < other.high) return false;
+			if (this.low < other.low && other.high < this.high) return true;
+			// corner case - one or both endpoints are the same (the other endpoint is covered)
+			if (this.low === other.low && this.lowInclude === false && other.lowInclude === true)
+				return false;
+			if (this.high === other.high && this.highInclude === false && other.highInclude === true)
+				return false;
+			return true;
+		};
+		equals (other) {
+			if (this.low !== other.low) return false;
+			if (this.high !== other.high) return false;
+			if (this.lowInclude !== other.lowInclude) return false;
+			if (this.highInclude !== other.highInclude) return false;
+			return true;
+		};
+		*/
+
+		coversPoint (point) {
+			return inside(point, this);
+		}
+
+
+		compare (other) {
+			return compare(this, other);
+		}
+
+		equals (other) {
+			return compare(this, other) == IntervalRelation.EQUALS;
+		}
+
+		outside (other) {
+			return [
+				IntervalRelation.OUTSIDE_LEFT,
+				IntervalRelation.OUTSIDE_RIGHT
+			].includes(compare(this, other));
+		}
+
+		overlap (other) {
+			return [
+				IntervalRelation.PARTIAL_LEFT,
+				IntervalRelation.PARTIAL_RIGHT
+			].includes(compare(this, other));	
+		}
+
+		covered (other) {
+			return compare(this, other) == IntervalRelation.COVERED;
+		}
+
+		covers (other) {
+			return compare(this, other) == IntervalRelation.COVERS;
+		}
+
+
+		/*
+			a.hasEndpointInside(b)
+
+			returns true if interval a has at least one endpoint inside interval b
+
+			This is easy for most intervals, but there are some subtleties
+			when when interval a and b have one or two endpoints in common
+
+			4 ways for intervals to share an endpoint
+
+			- a.high == b.low :
+				><  (a.high outside b)
+				>[  (a.high outside b)
+				]<  (a.high outside b)
+				][  (a.high inside b)
+			- a.high == b.high:
+				>>  (a.high inside b)
+				>]  (a.high inside b)
+				]>  (a.high outside b)
+				]]  (a.high inside b)
+			- a.low == b.low :
+				<<  (a.low inside b)
+				<[  (a.low inside b)
+				[<  (a.low outside b)
+				[[  (a.low inside b)
+			- a.low == b.high :
+				<>  (a.low outside b)
+				[>  (a.low outside b)
+				<]  (a.low outside b)
+				[]  (a.low inside b)
+
+		*/
+
+		/*
+		hasEndpointInside (b) {
+			const a = this;
+			// check if a is to the right of b
+			if (b.high < a.low) return false;
+			// check if a is to the left of b
+			if (a.high < b.low) return false;
+			// check if a.low is inside b
+			if (b.low < a.low && a.low < b.high) return true;
+			// check if a.high is inside b
+			if (b.low < a.high && a.high < b.high) return true;
+
+			// special consideration if a and b share endpoint(s)
+
+			// a.high shared
+			if (a.high == b.low) {
+				if (a.highInclude && b.lowInclude) return true;
+			}
+			if (a.high == b.high) {
+				if (!(a.highInclude && !b.highInclude)) return true;
+			}
+			// a.low shared
+			if (a.low == b.low) {
+				if (!(a.lowInclude && !b.lowInclude)) return true;
+			}
+			if (a.low == b.high) {
+				if (a.lowInclude && b.highInclude) return true;
+			}
+			return false;
+		};
+
+		*/
+
+	}
+
+
+
+	/*********************************************************
+	COMPARE BY INTERVAL ENDPOINTS
+	**********************************************************
+	
+	cmp functions for sorting intervals (ascending) based on
+	endpoint low or high
+
+	use with array.sort()
+
+	**********************************************************/
+
+	function _make_interval_cmp(low) {
+		return function cmp (a, b) {
+			let e1, e2;
+			if (low) {
+				e1 = [a.low, false, a.lowInclude]
+				e2 = [b.low, false, b.lowInclude]
+			} else {
+				e1 = [a.high, true, a.highInclude]
+				e2 = [b.high, true, b.highInclude]
+			}
+			return endpoint.compare(e1, e2);
+		}
+	}
+
+
+	/*
+		Add static variables to Interval class.
+	*/
+	Interval.cmpLow = _make_interval_cmp(true);
+	Interval.cmpHigh = _make_interval_cmp(false);
+	Interval.IntervalRelation = IntervalRelation;
+	Interval.endpoint = endpoint;
+
 	/*
 		Possibility for more interval methods such as union, intersection,
 	*/
 
-	return {
-		Interval: Interval,
-		endpoint: endpoint,
-		inside: inside,
-		compare: compare,
-		IntervalRelation: IntervalRelation
-	}
+	return Interval;
 });
 
