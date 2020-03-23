c73c3d006c815a984d018fcfc14bc9eed0a020c6
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-03-23 22:19:55 +0100

    got timeouts working in schedule

---
+++ v2.1/sequencing/schedule.js
@@ -0,0 +1,65 @@
+define(function(require) {
+
+    const motionutils = require("../util/motionutils");
+    const Interval = require("../util/interval");
+
+
+    class Schedule {
+
+        constructor(to) {
+            this.to = to;
+            this.tid = undefined;
+
+            // kickoff
+            let self = this;
+            this.to.ready.then(function(){
+                self.main();
+            });
+        }
+
+        clearTimeout() {
+            clearTimeout(this.tid);
+            this.tid = undefined;
+        }
+
+        /*
+            set timeout to point in time given in seconds
+        */
+        setTimeout(ts) {
+            clearTimeout(this.tid);
+            let self = this;
+            let delay = Math.max(ts - this.to.clock.now(), 0) * 1000;
+            this.tid = setTimeout(function(ts) {
+                self.onTimeout(ts);
+            }, delay, ts);
+        }
+
+        onTimeout(ts) {
+            this.tid = undefined;
+            // check if timeout was too early
+            let now = this.to.clock.now()
+            if (now < ts) {
+                // schedule new timeout
+                this.setTimeout(ts);
+            } else {
+                // handle timeout
+                this.main(now, true);
+            }
+        }
+
+        main(now, is_timeout) {
+            now = now || this.to.clock.now();
+
+            // do something
+            console.log("main", now);
+
+            // timeout 5 sec
+            let ts = now + 5;
+            this.setTimeout(ts);
+        }
+
+    }
+
+    return Schedule;
+});
+
---
+++ v2.1/test/main.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+    return {
+        motionutils: require("../util/motionutils"),
+        eventify: require("../util/eventify"),
+        Interval: require("../util/interval"),
+        Axis: require("../sequencing/axis"),
+        Schedule: require("../sequencing/schedule"),
+        TimingObject: require("../timingobject/timingobject").TimingObject
+    };
+});
+
