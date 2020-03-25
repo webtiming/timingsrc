cd69a0a63b33ce4e466d3da297265b3af028175a
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-03-25 09:12:49 +0100

    some tidying up in schedule

--- v2.1/sequencing/schedule.js
+++ v2.1/sequencing/schedule.js
@@ -6,61 +6,94 @@ define(function(require) {
 
     class Schedule {
 
-        constructor(to) {
+        // Default lookahead in seconds
+        static LOOKAHEAD = 5
+
+        // Run flags
+        static RUN_START = 1;
+        static RUN_TIMEOUT = 2;
+        static RUN_STOP = 3;
+
+
+        constructor(to, options) {
             this.to = to;
             this.tid = undefined;
-        }
+            this.running = false;
+            // options
+            options = options || {};
+            options.lookahead = options.lookahead || Schedule.LOOKAHEAD;
+            this.options = options;
 
-        clearTimeout() {
-            clearTimeout(this.tid);
-            this.tid = undefined;
         }
 
         /*
-            set timeout to point in time given in seconds
+            set timeout to point in time (seconds)
         */
         setTimeout(ts) {
-            clearTimeout(this.tid);
-            let self = this;
+            if (this.tid != undefined) {
+                throw new Error("at most on timeout");
+            }
             let delay = Math.max(ts - this.to.clock.now(), 0) * 1000;
-            this.tid = setTimeout(function(ts) {
-                self.onTimeout(ts);
-            }, delay, ts);
+            this.tid = setTimeout(this.onTimeout.bind(this), delay, ts);
         }
 
+        /*
+            handle timeout intended for point in time (seconds)
+        */
         onTimeout(ts) {
-            this.tid = undefined;
-            // check if timeout was too early
-            let now = this.to.clock.now()
-            if (now < ts) {
-                // schedule new timeout
-                this.setTimeout(ts);
-            } else {
-                // handle timeout
-                this.main(now, true);
+            if (this.tid != undefined) {
+                this.tid = undefined;
+                // check if timeout was too early
+                let now = this.to.clock.now()
+                if (now < ts) {
+                    // schedule new timeout
+                    this.setTimeout(ts);
+                } else {
+                    // handle timeout
+                    this.run(now, Schedule.RUN_TIMEOUT);
+                }
             }
         }
+
+        /*
+            start schedule
+        */
 
         start() {
-            if (this.tid == undefined) {
-                this.main();
+            if (!this.running) {
+                this.running = true;
+                this.run(this.to.clock.now(), Schedule.RUN_START);
             }
         }
 
+        /*
+            stop schedule
+        */
+
         stop() {
-            this.clearTimeout();
+            if (this.running) {
+                this.running = false;
+                clearTimeout(this.tid);
+                this.tid = undefined;
+                this.run(this.to.clock.now(), Schedule.RUN_STOP);
+            }
         }
 
-
-        main(now, is_timeout) {
-            now = now || this.to.clock.now();
+        /*
+            run schedule
+        */
+        run(now, run_flag) {
 
             // do something
-            console.log("main", now);
+            console.log("run", now, run_flag);
 
-            // timeout 5 sec
-            let ts = now + 5;
-            this.setTimeout(ts);
+
+            if (run_flag != Schedule.RUN_STOP) {
+                // timeout 5 sec
+                let ts = now + 5;
+                this.setTimeout(ts);
+            }
+
         }
 
     }
