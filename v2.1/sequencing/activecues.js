336c3b700e28433dbeb4f6015191dfed9e1e5b03
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-04-26 13:23:23 +0200

    fixed activecues jumping

--- v2.1/sequencing/activecues.js
+++ v2.1/sequencing/activecues.js
@@ -13,9 +13,14 @@ define(function(require) {
         constructor (axis, toA, toB) {
             this._axis = axis;
             this._toA = toA;
-            this._toA_ready = false;
             this._toB = toB;
+
+            // readiness
+            this._ready = new eventify.EventBoolean(false, {init:true});
             this._toB_ready = false;
+            this._toA_ready = false;
+
+            // activeCues
             this._activeCues = new Map(); // (key -> cue)
 
             // Register axis callback
@@ -32,71 +37,82 @@ define(function(require) {
 
             It is safe to use the same handler for multiple event sources.
 
-            Additionally we also use the trick of not specifying a context
-            for the handler. This implies that the this object will be
-            the event source, which we need to distinguish between the
-            two timingobjects.
+            Additionally we specify a context for the handler simply to
+            be able to use 'this' safely inside the wrapper implementation.
+
+            Since the same wrapper is registered with two distinct
+            event sources (timing objects), we use 'eInfo.src' to distinguish
+            between them.
 
             */
 
             this._onTimingUpdateWrapper = function (eInfo) {
                 let to = eInfo.src;
-                if (to == this._toA && !this._toA_ready) {
-                    this._toA_ready = true;
+                // check and set sequencer readiness
+                if (!this._ready.value) {
+                    if (to == this._toA) {
+                        this._toA_ready = true;
+                    } else if (to == this._toB) {
+                        this._toB_ready = true;
+                    }
+                    if (this._toA_ready && this._toB_ready) {
+                        this._ready.value = true;
+                    } else {
+                        return;
+                    }
                 }
-                if (to == this._toB && !this.toB_ready) {
-                    this._toB_ready = true;
-                }
-                if (this._toA_ready && this._toB_ready) {
-                    this._onTimingUpdate(eInfo);
-                }
+                this._onTimingUpdate(eInfo);
             };
             this._toA.on("change", this._onTimingUpdateWrapper, this);
             this._toB.on("change", this._onTimingUpdateWrapper, this);
 
 
             // Change event
-            eventify.eventifyInstance(this, {init:false});
+            eventify.eventifyInstance(this, {init:false}); // no events type
             this.eventifyDefineEvent("change", {init:true});
         }
 
-        /* Eventify */
+        /*
+            Eventify: callback formatter
+        */
         eventifyCallbackFormatter = function (type, e, eInfo) {
             return [e, eInfo];
         };
-
 
         /*
             Eventify: immediate events
         */
         eventifyMakeInitEvents = function (type) {
             if (type === "change") {
-                // todo : create eventMap from active cues
-                return (this.isReady()) ? [undefined] : [];
+                if (this.isReady()) {
+                    let changeMap = [...this._activeCues.values()].map(cue => {
+                        return [cue.key, {new:cue, old:undefined}];
+                    });
+                    return [changeMap];
+                }
             }
             return [];
         };
 
 
-
-
         /*
-            Ready State
+            Sequencer Ready Promise
         */
-
         get ready () {
-            return Promise.all([this._toA.ready, this._toB.ready]);
+            return eventify.makeEventPromise(this._ready, true);
         };
 
+        /*
+            Sequencer Read State
+        */
         isReady() {
-            return this._toA.isReady() && this._toB.isReady();
+            return this._ready.value;
         }
 
 
         /*
             Handling Axis Update Callbacks
         */
-
         _onAxisUpdate(batchMap) {
             // Do something
             console.log("onAxisUpdate");
@@ -105,7 +121,6 @@ define(function(require) {
         /*
             Handling Change Events from Timing Objects
         */
-
         _onTimingUpdate (eInfo) {
             const PosDelta = motionutils.MotionDelta.PosDelta;
             const MoveDelta = motionutils.MotionDelta.MoveDelta;
@@ -147,77 +162,80 @@ define(function(require) {
             */
             let delta = new motionutils.MotionDelta(to.old_vector, new_vector);
 
-
-            console.log(delta.toString());
-            console.log(to.old_vector);
-            console.log(to.vector);
-            console.log(new_vector);
-
             /*
                 Jump
             */
-            let change;
+
+            let activeCues, enterCues, exitCues;
 
 
             if (delta.posDelta == PosDelta.CHANGE) {
+                console.log("jump");
                 // make position interval
                 let low = Math.min(new_vector.position, other_new_vector.position);
                 let high = Math.max(new_vector.position, other_new_vector.position);
                 let itv = new Interval(low, high, true, true);
-                console.log("jump");
-                console.log(itv.toString());
-                change = this.jumpTo(itv);
+
+
+                // new active cues
+                activeCues = new Map(this._axis.lookup(itv).map(cue => {
+                    return [cue.key, cue];
+                }));
+                // exit cues - in old activeCues but not in new activeCues
+                exitCues = util.map_difference(this._activeCues, activeCues);
+                // enter cues - not in old activeCues but in new activeCues
+                enterCues = util.map_difference(activeCues, this._activeCues);
+            }
+
+            /*
+                Moving
+            */
+
+
+
+
+            // update active cues
+            this._activeCues = activeCues;
+
+
+            // make change Map
+            let changeMap = new Map();
+            for (let cue of exitCues.values()) {
+                changeMap.set(cue.key, {new:undefined, old:cue});
+            }
+            for (let cue of enterCues.values()) {
+                changeMap.set(cue.key, {new:cue, old:undefined});
             }
 
             // event notification
-            this.eventifyTriggerEvent("change", change);
-
+            this.eventifyTriggerEvent("change", changeMap);
         };
 
 
 
-
-
         /*
-            This calculates the transition in activeCues caused by
-            a jump on the timeline.
-            interval may be point or range
+            Map accessors
         */
 
-        jumpTo (interval) {
+        has(key) {
+            return this._activeCues.has(key);
+        };
 
+        get(key) {
+            return this._activeCues.get(key);
+        };
 
-            let newActiveCues = new Map(this._axis.lookup(interval).map(cue => {
-                return [cue.key, cue];
-            }));
-
-            /*
-                find exit cues
-                were in old active cues - but not in new
-            */
-            let exitCues = util.map_difference(this._activeCues, newActiveCues);
-            /*
-                find enter cues
-                were not in old active cues - but are in new
-            */
-            let enterCues = util.map_difference(newActiveCues, this._activeCues);
-
-            // update active cues
-            this._activeCues = newActiveCues;
-
-            return {
-                exitCues: exitCues,
-                enterCues: enterCues
-            };
-
-        }
-
-
+        keys() {
+            return this._activeCues.keys();
+        };
 
         values() {
             return this._activeCues.values();
-        }
+        };
 
+        entries() {
+            return this._activeCues.entries();
+        }
 
     }
 
