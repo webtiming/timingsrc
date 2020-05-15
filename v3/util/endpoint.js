cb9d2bc4f7ef111ab4667f07e6cdc4792431f44f
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-05-15 21:16:53 +0200

    fixed up interval to use modified endpoints

--- v3/util/endpoint.js
+++ v3/util/endpoint.js
@@ -43,17 +43,34 @@ define(function () {
 	*/
 	const MODE_RIGHT_OPEN = 0;
 	const MODE_LEFT_CLOSED = 1;
-	const MODE_SINGULAR = 2;
+	const MODE_POINT = 2;
 	const MODE_RIGHT_CLOSED = 3;
 	const MODE_LEFT_OPEN = 4;
+
+	// create endpoint
+	function create(val, right, closed, singular) {
+		// make sure infinity endpoints are legal
+		if (val == Infinity) {
+			if (right == false || closed == false) {
+				throw new Error("Infinity endpoint must be right-closed or singular");
+			}
+		}
+		if (val == -Infinity) {
+			if (right == true || closed == false) {
+				throw new Error("-Infinity endpoint must be left-closed or singular")
+			}
+		}
+		return [val, right, closed, singular];
+	}
+
 
 	/*
 		resolve endpoint mode
 	*/
 	function get_mode(e) {
 		let [val, right, closed, singular] = e;
-		if (singular) {
-			return MODE_SINGULAR;
+		if (right == undefined) {
+			return MODE_POINT;
 		} else if (right) {
 			if (closed) {
 				return MODE_RIGHT_CLOSED;
@@ -99,13 +116,13 @@ define(function () {
 			if (!isNumber(e1)) {
 				throw new Error("e1 not a number", e1);
 			}
-			e1 = [e1, undefined, undefined, true];
+			e1 = create(e1, undefined, undefined, true);
 		}
 		if (e2.length === undefined) {
 			if (!isNumber(e2)) {
 				throw new Error("e2 not a number", e2);
 			}
-			e2 = [e2, undefined, undefined, true];
+			e2 = create(e2, undefined, undefined, true);
 		}
 		if (e1[0] != e2[0]) {
 			// different values
@@ -169,11 +186,14 @@ define(function () {
 		} else {
 			let mode = get_mode(e);
 			let val = e[0];
+			if (val == Infinity || val == -Infinity) {
+				val = "--";
+			}
 			if (mode == MODE_RIGHT_OPEN) {
 				return `${val})`
 			} else if (mode == MODE_LEFT_CLOSED) {
 				return `[${val}`
-			} else if (mode == MODE_SINGULAR){
+			} else if (mode == MODE_POINT){
 				return `[${val}]`
 			} else if (mode == MODE_RIGHT_CLOSED) {
 				return `${val}]`
@@ -188,6 +208,7 @@ define(function () {
 		toString: toString,
 		equals: equals,
 		rightof: rightof,
-		leftof: leftof
+		leftof: leftof,
+		create: create
 	}
 });
--- v3/util/interval.js
+++ v3/util/interval.js
@@ -1,216 +1,121 @@
 /*
-	Copyright 2015 Norut Northern Research Institute
+	Copyright 2020
 	Author : Ingar Mæhlum Arntzen
 
-  This file is part of the Timingsrc module.
+	This file is part of the Timingsrc module.
 
-  Timingsrc is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+	Timingsrc is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-  Timingsrc is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
+	Timingsrc is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public License
-  along with Timingsrc.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Lesser General Public License
+	along with Timingsrc.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-define(function () {
+define(function(require) {
 
 	'use strict';
 
+	const endpoint = require("./endpoint");
 
 	const isNumber = function(n) {
 		let N = parseFloat(n);
 	    return (n===N && !isNaN(N));
 	};
 
-
 	/*********************************************************
-
-	ENDPOINT
-
-	Utilities for interval endpoints comparisons
-
+	INTERVAL ERROR
 	**********************************************************/
 
-	const endpoint = function () {
-
-		// bit flag
-		const CLOSED_BIT = 0
-		const RIGHT_BIT = 1
-
-		// endpoint mode
-		const MODE_LEFT_OPEN = 0;     //bx000
-		const MODE_LEFT_CLOSED = 1;   //bx001
-		const MODE_RIGHT_OPEN = 2;    //bx010
-		const MODE_RIGHT_CLOSED = 3;  //bx011
-		const MODE_VALUE = 4          //bx100
-
-		/*
-			endpoint order
-			p), [p, p, p], (p
-		*/
-		const order = [];
-
-		order[MODE_RIGHT_OPEN] = 0;
-		order[MODE_LEFT_CLOSED] = 1;
-		order[MODE_VALUE] = 2;
-		order[MODE_RIGHT_CLOSED] = 3;
-		order[MODE_LEFT_OPEN] = 4;
-
-		/*
-			get order
-
-			given two endpoints return
-			two numbers representing their order
-			also accepts regular numbers as endpoints
-
-			for points or endpoint values that are not
-			equal, these values convey order directly,
-			otherwise the order numbers 0-4 are returned
-			based on endpoint inclusion and direction
-
-			parameters are either
-			- point (number)
-			- endpoint [value (number), right (bool), closed (bool)]
-		*/
-
-		function get_order(e1, e2) {
-
-			// support plain numbers (not endpoints)
-			if (e1.length === undefined) {
-				if (!isNumber(e1)) {
-					throw new Error("e1 not a number", e1);
-				}
-				e1 = [e1, undefined, undefined];
-			}
-			if (e2.length === undefined) {
-				if (!isNumber(e2)) {
-					throw new Error("e2 not a number", e2);
-				}
-				e2 = [e2, undefined, undefined];
-			}
-
-			let [e1_val, e1_right, e1_closed] = e1;
-			let [e2_val, e2_right, e2_closed] = e2;
-			let e1_mode, e2_mode;
-
-			if (e1_val != e2_val) {
-				// different values
-				return [e1_val, e2_val];
-			} else {
-				// equal values
-				if (e1_closed === undefined) {
-					e1_mode = MODE_VALUE;
-				} else {
-					e1_closed = Boolean(e1_closed);
-					e1_right = Boolean(e1_right);
-					e1_mode = (+e1_closed << CLOSED_BIT) | (+e1_right << RIGHT_BIT);
-				}
-				if (e2_closed === undefined) {
-					e2_mode = MODE_VALUE;
-				} else {
-					e2_closed = Boolean(e2_closed);
-					e2_right = Boolean(e2_right);
-					e2_mode = (+e2_closed << CLOSED_BIT) | (+e2_right << RIGHT_BIT);
-				}
-				return [order[e1_mode], order[e2_mode]];
-			}
-		}
-
-		/*
-			return true if e1 is ordered before e2
-			false if equal
-		*/
-
-		function leftof(e1, e2) {
-			let [order1, order2] = get_order(e1, e2);
-			return (order1 < order2);
-		}
-
-		/*
-			return true if e1 is ordered after e2
-			false is equal
-		*/
-
-		function rightof(e1, e2) {
-			let [order1, order2] = get_order(e1, e2);
-			return (order1 > order2);
-		}
-
-		/*
-			return true if e1 is ordered equal to e2
-		*/
-
-		function equals(e1, e2) {
-			let [order1, order2] = get_order(e1, e2);
-			return (order1 == order2);
-		}
-
-		/*
-			return -1 if ordering e1, e2 is correct
-			return 0 if e1 and e2 is equal
-			return 1 if ordering e1, e2 is incorrect
-		*/
-
-		function compare(e1, e2) {
-			let [order1, order2] = get_order(e1, e2);
-			let diff = order1 - order2;
-			if (diff == 0) return 0;
-			return (diff > 0) ? 1 : -1;
-		}
-
-		/*
-			human friendly endpoint representation
-		*/
-		function toString(e) {
-			if (e.length === undefined) {
-				return e.toString();
-			} else {
-				let [val, right, closed] = e;
-				let s = val.toString()
-				if (right && closed) {
-					return s + "]";
-				} else if (right && !closed) {
-					return s + ")";
-				} else if (!right && closed) {
-					return "[" + s;
-				} else {
-					return "(" + s;
-				}
-			}
-		}
-
-		return {
-			compare: compare,
-			toString: toString,
-			equals: equals,
-			rightof: rightof,
-			leftof: leftof
-		}
-
-	}();
+	var IntervalError = function (message) {
+		this.name = "IntervalError";
+		this.message = (message||"");
+	};
+	IntervalError.prototype = Error.prototype;
 
 
 	/*********************************************************
-	INSIDE INTERVAL
-	**********************************************************
-
-	inside (e, interval)
-
-	return true if given point/endpoint e is inside interval
-
+	INTERVAL
 	**********************************************************/
 
-	function inside (e, interval) {
-		let leftof = endpoint.leftof(e, interval.endpointLow);
-		let rightof = endpoint.rightof(e, interval.endpointHigh);
-		return !leftof && !rightof;
+	class Interval {
+
+		constructor (low, high, lowInclude, highInclude) {
+			var lowIsNumber = isNumber(low);
+			var highIsNumber = isNumber(high);
+			// new Interval(3.0) defines singular - low === high
+			if (lowIsNumber && high === undefined) high = low;
+			if (!isNumber(low)) throw new IntervalError("low not a number");
+			if (!isNumber(high)) throw new IntervalError("high not a number");
+			if (low > high) throw new IntervalError("low > high");
+			if (low === high) {
+				lowInclude = true;
+				highInclude = true;
+			}
+			if (low === -Infinity) lowInclude = true;
+			if (high === Infinity) highInclude = true;
+			if (lowInclude === undefined) lowInclude = true;
+			if (highInclude === undefined) highInclude = false;
+			if (typeof lowInclude !== "boolean") throw new IntervalError("lowInclude not boolean");
+			if (typeof highInclude !== "boolean") throw new IntervalError("highInclude not boolean");
+			this.low = low;
+			this.high = high;
+			this.lowInclude = lowInclude;
+			this.highInclude = highInclude;
+			this.length = this.high - this.low;
+			this.singular = (this.low === this.high);
+			this.finite = (isFinite(this.low) && isFinite(this.high));
+
+			/*
+				Accessors for full endpoint representationo
+				[value (number), right (bool), closed (bool)]
+
+				- use with inside(endpoint, interval)
+			*/
+			this.endpointLow = endpoint.create(this.low, false, this.lowInclude, this.singular);
+			this.endpointHigh = endpoint.create(this.high, true, this.highInclude, this.singular);
+		}
+
+		toString () {
+			const toString = endpoint.toString;
+			if (this.singular) {
+				return toString(this.endpointLow);
+			} else {
+				let low = endpoint.toString(this.endpointLow);
+				let high = endpoint.toString(this.endpointHigh);
+				return `${low},${high}`;
+			}
+		};
+
+		covers_endpoint (p) {
+			let leftof = endpoint.leftof(p, this.endpointLow);
+			let rightof = endpoint.rightof(p, this.endpointHigh);
+			return !leftof && !rightof;
+		}
+
+		compare (other) {
+			return compare(this, other);
+		}
+
+		equals (other) {
+			return compare(this, other) == Relation.EQUALS;
+		}
+
+		covered (other) {
+			return compare(this, other) == Relation.COVERED;
+		}
+
+		covers (other) {
+			return compare(this, other) == Relation.COVERS;
+		}
 	}
+
 
 
 	/*********************************************************
@@ -276,8 +181,8 @@ define(function () {
 			}
 		}
 
-		let cmp_1 = endpoint.compare(a.endpointLow, b.endpointLow);
-		let cmp_2 = endpoint.compare(a.endpointHigh, b.endpointHigh);
+		let cmp_1 = endpoint.cmp(a.endpointLow, b.endpointLow);
+		let cmp_2 = endpoint.cmp(a.endpointHigh, b.endpointHigh);
 		let key = cmp_1*10 + cmp_2;
 
 		if (key == 11) {
@@ -305,94 +210,8 @@ define(function () {
 	}
 
 
-
 	/*********************************************************
-	INTERVAL ERROR
-	**********************************************************/
-
-	var IntervalError = function (message) {
-		this.name = "IntervalError";
-		this.message = (message||"");
-	};
-	IntervalError.prototype = Error.prototype;
-
-
-	/*********************************************************
-	INTERVAL
-	**********************************************************/
-
-	class Interval {
-
-		constructor (low, high, lowInclude, highInclude) {
-			var lowIsNumber = isNumber(low);
-			var highIsNumber = isNumber(high);
-			// new Interval(3.0) defines singular - low === high
-			if (lowIsNumber && high === undefined) high = low;
-			if (!isNumber(low)) throw new IntervalError("low not a number");
-			if (!isNumber(high)) throw new IntervalError("high not a number");
-			if (low > high) throw new IntervalError("low > high");
-			if (low === high) {
-				lowInclude = true;
-				highInclude = true;
-			}
-			if (low === -Infinity) lowInclude = true;
-			if (high === Infinity) highInclude = true;
-			if (lowInclude === undefined) lowInclude = true;
-			if (highInclude === undefined) highInclude = false;
-			if (typeof lowInclude !== "boolean") throw new IntervalError("lowInclude not boolean");
-			if (typeof highInclude !== "boolean") throw new IntervalError("highInclude not boolean");
-			this.low = low;
-			this.high = high;
-			this.lowInclude = lowInclude;
-			this.highInclude = highInclude;
-			this.length = this.high - this.low;
-			this.singular = (this.low === this.high);
-			this.finite = (isFinite(this.low) && isFinite(this.high));
-
-			/*
-				Accessors for full endpoint representationo
-				[value (number), right (bool), closed (bool)]
-
-				- use with inside(endpoint, interval)
-			*/
-			this.endpointLow = [this.low, false, this.lowInclude];
-			this.endpointHigh = [this.high, true, this.highInclude];
-		}
-
-		toString () {
-			var lowBracket = (this.lowInclude) ? "[" : "(";
-			var highBracket = (this.highInclude) ? "]" : ")";
-			var low = (this.low === -Infinity) ? "[--" : this.low; //.toFixed(2);
-			var high = (this.high === Infinity) ? "--]" : this.high; //.toFixed(2);
-			if (this.singular)
-				return lowBracket + low + highBracket;
-			return lowBracket + low + ',' + high + highBracket;
-		};
-
-		compare (other) {
-			return compare(this, other);
-		}
-
-		inside (p) {
-			return inside(p, this);
-		}
-
-		equals (other) {
-			return compare(this, other) == Relation.EQUALS;
-		}
-
-		covered (other) {
-			return compare(this, other) == Relation.COVERED;
-		}
-
-		covers (other) {
-			return compare(this, other) == Relation.COVERS;
-		}
-	}
-
-
-	/*********************************************************
-	COMPARE BY INTERVAL ENDPOINTS
+	COMPARE INTERVALS BY ENDPOINT
 	**********************************************************
 
 	cmp functions for sorting intervals (ascending) based on
@@ -406,13 +225,13 @@ define(function () {
 		return function cmp (a, b) {
 			let e1, e2;
 			if (low) {
-				e1 = [a.low, false, a.lowInclude]
-				e2 = [b.low, false, b.lowInclude]
+				e1 = [a.low, false, a.lowInclude, a.singular];
+				e2 = [b.low, false, b.lowInclude, a.singular];
 			} else {
-				e1 = [a.high, true, a.highInclude]
-				e2 = [b.high, true, b.highInclude]
+				e1 = [a.high, true, a.highInclude, a.singular];
+				e2 = [b.high, true, b.highInclude, a.singular];
 			}
-			return endpoint.compare(e1, e2);
+			return endpoint.cmp(e1, e2);
 		}
 	}
 
@@ -423,7 +242,6 @@ define(function () {
 	Interval.Relation = Relation;
 	Interval.cmpLow = _make_interval_cmp(true);
 	Interval.cmpHigh = _make_interval_cmp(false);
-	Interval.endpoint = endpoint;
 
 	/*
 		Possibility for more interval methods such as union, intersection,
