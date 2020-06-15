2e95ff8b06341fe2345c00ebcd51bac491ad64a1
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-06-15 21:17:02 +0200

    started working on doublesequencer

--- v3/sequencing/doublesequencer.js
+++ v3/sequencing/doublesequencer.js
@@ -20,147 +20,91 @@
 
 define(function(require) {
 
-    const Interval = require("../util/interval");
     const util = require("../util/util");
+    const endpoint = require("../util/endpoint");
+    const Interval = require("../util/interval");
     const motionutils = require("../util/motionutils");
     const eventify = require("../util/eventify");
     const Schedule = require("./schedule");
+    const Axis = require("./axis");
 
-
+    const PosDelta = motionutils.MotionDelta.PosDelta;
+    const MoveDelta = motionutils.MotionDelta.MoveDelta;
 
     class DoubleSequencer {
 
         constructor (axis, toA, toB) {
-            this._axis = axis;
-            this._toA = toA;
-            this._toB = toB;
 
-            // readiness
-            this._ready = new eventify.EventBoolean(false, {init:true});
-            this._toB_ready = false;
-            this._toA_ready = false;
-
-            // schedules
-            this._scheduleA;
-            this._scheduleB;
-
-            // activeCues
+            // ActiveCues
             this._activeCues = new Map(); // (key -> cue)
 
-            // Register axis callback
-            this._handle = this._axis.add_callback(this._onAxisUpdate.bind(this));
+            // Axis
+            this._axis = axis;
+            let axis_cb = this._onAxisCallback.bind(this)
+            this._axis_cb = this._axis.add_callback(axis_cb);
 
-            /*
+            // Timing objects
+            this._toA = toA;
+            this._toB = toB;
+            let to_cb = this._onTimingCallback.bind(this);
+            this._subA = this._toA.on("timingsrc", to_cb);
+            this._subB = this._toB.on("timingsrc", to_cb);
 
-            Wrap onTimingUpdate
-
-            To allow multiple instances of ActiveCues to subscribe to
-            the same event source, the handler must be different objects.
-            If we use the class method directly as handler, it will
-            be on the prototype and therefor be shared between instances.
-
-            It is safe to use the same handler for multiple event sources.
-
-            Additionally we specify a context for the handler simply to
-            be able to use 'this' safely inside the wrapper implementation.
-
-            Since the same wrapper is registered with two distinct
-            event sources (timing objects), we use 'eInfo.src' to distinguish
-            between them.
-
-            */
-
-            this._onTimingUpdateWrapper = function (eInfo) {
-                let to = eInfo.src;
-                let new_vector;
-                // check and set sequencer readiness
-                if (!this._ready.value) {
-                    if (to == this._toA) {
-                        this._toA_ready = true;
-                        this._scheduleA = new Schedule(to.clock, to.range,
-                                                       this._axis, this._onCallback);
-                    } else if (to == this._toB) {
-                        this._toB_ready = true;
-                        this._scheduleB = new Schedule(to.clock, to.range,
-                                                       this._axis, this._onCallback);
-                    }
-                    if (this._toA_ready && this._toB_ready) {
-                        this._ready.value = true;
-                    }
-                }
-
-                /*
-                    If update is the initial vector from the timing object,
-                    we set current time as the official time for the update.
-                    Else, the new vector is "live" and we use the timestamp
-                    when it was created as the official time for the update.
-                    This is represented by the new_vector.
-                */
-                if (eInfo.init) {
-                    new_vector = motionutils.calculateVector(to.vector, to.clock.now());
-                } else {
-                    new_vector = to.vector;
-                }
-
-                if (this.isReady()) {
-                    this._onTimingUpdate(to, new_vector);
-                }
-            };
-            this._toA.on("change", this._onTimingUpdateWrapper, this);
-            this._toB.on("change", this._onTimingUpdateWrapper, this);
-
-            // Schedule Callback
-            this._onCallback = function(eList, src) {
-                this._onScheduleCallback(eList, src);
-            }.bind(this);
+            // Schedules
+            let sched_cb = this._onScheduleCallback.bind(this);
+            this._schedA = new Schedule(this._axis, toA);
+            this._schedA_cb = this._schedA.add_callback(sched_cb);
+            this._schedB = new Schedule(this._axis, toB);
+            this._schedB_cb = this._schedB.add_callback(sched_cb);
 
             // Change event
-            eventify.eventifyInstance(this, {init:false}); // no events type
+            eventify.eventifyInstance(this);
             this.eventifyDefineEvent("change", {init:true});
         }
 
-        /*
-            Eventify: callback formatter
-        */
-        eventifyCallbackFormatter = function (type, e, eInfo) {
-            return [e, eInfo];
-        };
+        /***************************************************************
+         EVENTS
+        ***************************************************************/
 
         /*
             Eventify: immediate events
         */
         eventifyMakeInitEvents = function (type) {
             if (type === "change") {
-                if (this.isReady()) {
-                    let changeMap = [...this._activeCues.values()].map(cue => {
-                        return [cue.key, {new:cue, old:undefined}];
+                if (this.isReady() && this._activeCues.size > 0) {
+                    let events = [...this._activeCues.values()].map(cue => {
+                        return {key:cue.key, new:cue, old:undefined};
                     });
-                    return [changeMap];
+                    return [true, events];
                 }
             }
             return [];
         };
 
 
+        /***************************************************************
+         READYNESS
+        ***************************************************************/
+
         /*
             Sequencer Ready Promise
         */
         get ready () {
-            return eventify.makeEventPromise(this._ready, true);
+            return Promise.all(this._toA.ready, this._toB_ready);
         };
 
         /*
             Sequencer Read State
         */
         isReady() {
-            return this._ready.value;
+            return this._toA.isReady() && this._toB.isReady();
         }
 
 
         /*
             Handling Axis Update Callbacks
         */
-        _onAxisUpdate(batchMap) {
+        _onAxisCallback(batchMap) {
             // Do something
             console.log("onAxisUpdate");
         }
@@ -168,18 +112,45 @@ define(function(require) {
         /*
             Handling Change Events from Timing Objects
         */
-        _onTimingUpdate (to, new_vector) {
-            const PosDelta = motionutils.MotionDelta.PosDelta;
-            const MoveDelta = motionutils.MotionDelta.MoveDelta;
+        _onTimingCallback (eArg, eInfo) {
+
+            const events = [];
+
+            if (!this.isReady()) {
+                return;
+            }
+
+            // TODO - avoid processing ready signal from both timing objects
+            // initially.
+
+
+            /* figure out which timing object was firing */
+            const to = eInfo.src;
             const other_to = (to == this._toA) ? this._toB : this._toA;
-
-            if (to == this._toA) {
+            if (eInfo.src == this._toA) {
                 console.log("update toA");
-            }
-            if (to == this._toB) {
+            } else {
                 console.log("update toB");
-
             }
+
+            /*
+                If update is the initial vector from the timing object,
+                we set current time as the official time for the update.
+                Else, the new vector is "live" and we use the timestamp
+                when it was created as the official time for the update.
+                This is represented by the new_vector.
+            */
+            let new_vector;
+            if (eArg.live) {
+                new_vector = to.vector;
+            } else {
+                new_vector = motionutils.calculateVector(to.vector, to.clock.now());
+            }
+
+            /*
+                The nature of the vector change
+            */
+            const delta = new motionutils.MotionDelta(to.old_vector, new_vector);
 
 
             /*
@@ -190,34 +161,42 @@ define(function(require) {
 
 
             /*
-                The nature of the vector change
+                Reevaluate active state.
+                This is required after any discontinuity of the position (jump),
+                or if the motion stopped without jumping (pause or halt at range
+                restriction)
             */
-            let delta = new motionutils.MotionDelta(to.old_vector, new_vector);
+            if (delta.posDelta == PosDelta.CHANGE || delta.MoveDelta == MoveDelta.STOP) {
 
-            /*
-                Jump
-            */
-
-            let activeCues, enterCues, exitCues;
-
-
-            if (delta.posDelta == PosDelta.CHANGE) {
-                console.log("jump");
                 // make position interval
                 let low = Math.min(new_vector.position, other_new_vector.position);
                 let high = Math.max(new_vector.position, other_new_vector.position);
                 let itv = new Interval(low, high, true, true);
 
-
                 // new active cues
-                activeCues = new Map(this._axis.lookup(itv).map(cue => {
+                let activeCues = new Map(this._axis.lookup(itv).map(cue => {
                     return [cue.key, cue];
                 }));
                 // exit cues - in old activeCues but not in new activeCues
-                exitCues = util.map_difference(this._activeCues, activeCues);
+                let exitCues = util.map_difference(this._activeCues, activeCues);
                 // enter cues - not in old activeCues but in new activeCues
-                enterCues = util.map_difference(activeCues, this._activeCues);
+                let enterCues = util.map_difference(activeCues, this._activeCues);
+                // update active cues
+                this._activeCues = activeCues;
+                // make events
+                for (let cue of exitCues.values()) {
+                    events.push({key:cue.key, new:undefined, old:cue});
+                }
+                for (let cue of enterCues.values()) {
+                    events.push({key:cue.key, new:cue, old:undefined});
+                }
+                // event notification
+                if (events.length > 0 ) {
+                    this.eventifyTriggerEvent("change", events);
+                }
             }
+
+
 
             /*
                 Moving
@@ -225,30 +204,15 @@ define(function(require) {
 
             // kick off schedule
 
+            /*
             // TODO - need to kick off the other as well
             if (to == this._toA) {
                 this._scheduleA.setVector(new_vector);
             } else if (to == this._toB) {
                 this._scheduleB.setVector(new_vector);
             }
+            */
 
-
-
-            // update active cues
-            this._activeCues = activeCues;
-
-
-            // make change Map
-            let changeMap = new Map();
-            for (let cue of exitCues.values()) {
-                changeMap.set(cue.key, {new:undefined, old:cue});
-            }
-            for (let cue of enterCues.values()) {
-                changeMap.set(cue.key, {new:cue, old:undefined});
-            }
-
-            // event notification
-            this.eventifyTriggerEvent("change", changeMap);
         };
 
 
