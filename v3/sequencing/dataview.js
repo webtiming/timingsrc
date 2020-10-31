2de57acf91f62b1acd89ac96167da892d4622629
Ingar Arntzen <ingar.arntzen@gmail.com>
2020-10-31 20:04:11 +0100

    fixed convert, datasetcallback

--- v3/sequencing/dataview.js
+++ v3/sequencing/dataview.js
@@ -1,32 +1,30 @@
 import ObservableMap from '../util/observablemap.js';
 
-/**
- * Read only wrapper around dataset.
- * Implements ObservableMap
- * Stateless - forwards to source dataset
- * 
- * Adds filter and map/convert functionality 
- * on cues in source dataset
- * 
- * 
- * Implements the same interface as Dataset
- * 
- * Inherit from ObservableMap, but need to
- * override or remove behavior.
- * 
- * 
- * 
- */
-
-
-
 /*
     Dataview provides read-only access to subset of a source Dataset
-    - restrictions in time
-    - restrictions (keep(cue))  key_filter, data_filter?
-    - 
-    - transformation (convert(cue)) - data_convert -- 
-        warning: value filter applies before convert 
+
+    - <interval> filter in time domain, 
+        include only cues that match given interval (default mode 62)
+
+    - <options>
+        - <key_filter> : filter by cue key
+            function keep(key) returns boolena
+        - <data_filter> : filter by cue data
+            function keep(data) returns boolean
+        - <data_convert> : change cue data
+            function convert(data) returns data
+            NOTE: filtering occurs BEFORE convert
+            and only impacts the presentation of cues
+            WARNING: it is possible to change the value
+            in such a way that filtering appears incorrect
+
+    This dataview implementation is STATELESS
+    It does not manage its own state, only implements a
+    stateless wrapper around its source dataset.
+
+    NOTE: The stateless nature also means that it must remain static.
+    Intervals and filters can not be changed.
+
 */
 
 class Dataview extends ObservableMap {
@@ -35,12 +33,14 @@ class Dataview extends ObservableMap {
         super();
         this._key_filter = options.key_filter;
         this._data_filter = options.data_filter;
+        this._data_convert = options.data_convert;
         this._interval = interval;
         this._size = 0;
 
         // Source Dataset
         this._src_ds = dataset;
-        this._src_ds.on("batch", this.onBatchCallback.bind(this));
+        let cb = this._onDatasetCallback.bind(this)
+        this._src_ds_cb = this._src_ds.add_callback(cb);
     }
 
     /** 
@@ -48,8 +48,14 @@ class Dataview extends ObservableMap {
     */
 
     _cue_keep(cue) {
+        if (cue == undefined) {
+            return false;
+        }
         // check interval
         if (this._interval) {
+            if (cue.interval == undefined) {
+                return false;
+            }
             if (!this._interval.match(cue.interval)) {
                 return false;
             }
@@ -69,11 +75,23 @@ class Dataview extends ObservableMap {
         return true;
     }
 
+    _cue_convert(cue) {
+        if (cue != undefined && this._data_convert) {
+            // copy
+            return {
+                key: cue.key,
+                interval: cue.interval,
+                data: this._data_convert(cue.data)
+            }
+        }
+        return cue;
+    }
+
     /**
      * Filter (and modify) event items based on key_filter and data_filter
      */
 
-    _items_filter(items) {
+    _items_filter_convert(items) {
         let _items = [];
         for (let item of items) {
             if (item.new == undefined && item.old == undefined) {
@@ -87,15 +105,20 @@ class Dataview extends ObservableMap {
             to be removed from the item - effectively turning the change
             operation into an add operation. 
             */
-            let _old = (item.old != undefined && this._cue_keep(item.old)) ? item.old : undefined;
-            let _new = (item.new != undefined && this._cue_keep(item.new)) ? item.new : undefined;
+            let _old = (this._cue_keep(item.old)) ? item.old : undefined;
+            let _new = (this._cue_keep(item.new)) ? item.new : undefined;
             if (_old == undefined && _new == undefined) {
                 continue;
             }
+            // convert
+            _old = this._cue_convert(_old);
+            _new = this._cue_convert(_new);
+            // push
             _items.push({key:item.key, new: _new, old: _old});
         }
         return _items;
     }
+
 
     /**
      * ObservableMap needs access to source dataset. 
@@ -116,8 +139,10 @@ class Dataview extends ObservableMap {
             } else {
                 cues = [...this.datasource.values()];
             }
-            // filter
-            cues = cues.filter(this._cue_keep, this);
+            // filter & convert
+            cues = cues.filter(this._cue_keep, this)
+                .map(this._cue_convert, this);
+
             // make event items
             let items = cues.map((cue) => {
                 return {key:cue.key, new:cue, old:undefined};
@@ -130,9 +155,12 @@ class Dataview extends ObservableMap {
 
     /*
         forward events
+
+        TODO - should be using the callback instead
     */
-    onBatchCallback(items) {
-        items = this._items_filter(items);
+    _onDatasetCallback(eventMap) {
+        let items = [...eventMap.values()];
+        items = this._items_filter_convert(items);
         // update size
         for (let item of items) {
             if (item.new != undefined && item.old == undefined) {
